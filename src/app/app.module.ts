bcd62ef93f9bbdc16429a000d17c992cfd9220ff
vux0501 <hvudtkt@gmail.com>
2024-01-10 13:48:08 +0700

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [HomeComponent],
