3d63a3ebbc01c9665626feaecf89fb5e650f17a9
vux0501 <hvudtkt@gmail.com>
2024-01-10 15:22:12 +0700

    confirm order page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -11,9 +13,11 @@ import { OrderComponent } from './order/order.component';
     HeaderComponent,
     FooterComponent,
     OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
   ],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [OrderComponent],
+  bootstrap: [OrderConfirmComponent],
 })
 export class AppModule {}
