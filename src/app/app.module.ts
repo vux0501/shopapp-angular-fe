9e0b173a3658a135e43dcb3824bad601c14bd6d2
vux0501 <hvudtkt@gmail.com>
2024-01-10 15:05:29 +0700

    Order detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [HomeComponent],
+  bootstrap: [OrderComponent],
 })
 export class AppModule {}
